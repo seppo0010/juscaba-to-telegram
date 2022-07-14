940a8fa61a4aa25efd0506f7f94cb740cb486a2f
Sebastian Waisbrot <seppo0010@gmail.com>
2022-07-14 16:58:33 -0300

    Use bigint for channel ids

--- database/migrations/0003_create_subscriptions.up.sql
+++ database/migrations/0003_create_subscriptions.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS channel_subscription (
-  channel_id INTEGER NOT NULL,
+  channel_id BIGINT NOT NULL,
   expediente_id VARCHAR(255) NOT NULL,
   UNIQUE (channel_id, expediente_id)
 );
